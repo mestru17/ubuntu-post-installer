25afb057545244ba1d3b0cce61635e9f8caf6e31
Mathias Estrup <mestru17@student.aau.dk>
2021-04-05 20:36:30 +0200

    Add some zsh plugin installs
    - Add zsh-autosuggestions install
    - Add zsh-syntax-highlighting install

--- ubuntu-post-installer.sh
+++ ubuntu-post-installer.sh
@@ -18,6 +18,12 @@ sudo chsh -s $(which zsh)
 # Oh My Zsh (better Zsh prompt)
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 # Bat (better version of cat)
 sudo apt install bat
 sudo echo 'alias bat="batcat"' >> ~/.zshrc
